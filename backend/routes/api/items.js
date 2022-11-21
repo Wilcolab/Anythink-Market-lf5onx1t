bd718ec1cf4cb97725b099a2c1341e3b6ad05029
sebastian <70783227+tamerlantian@users.noreply.github.com>
2022-11-21 13:33:54 +0000

    added filtering option to items API endpoint

--- backend/routes/api/items.js
+++ backend/routes/api/items.js
@@ -57,6 +57,7 @@ router.get("/", auth.optional, function(req, res, next) {
     query.tagTitle = { $in: [req.query.title]}
   }
 
+
   Promise.all([
     req.query.seller ? User.findOne({ username: req.query.seller }) : null,
     req.query.favorited ? User.findOne({ username: req.query.favorited }) : null
