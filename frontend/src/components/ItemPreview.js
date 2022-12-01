bd3b0ec7aa68678a09c73d3f682a484aa5c26919
sebastian <70783227+tamerlantian@users.noreply.github.com>
2022-12-01 16:57:09 +0000

    import png fixed

--- frontend/src/components/ItemPreview.js
+++ frontend/src/components/ItemPreview.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import agent from "../agent";
 import { connect } from "react-redux";
 import { ITEM_FAVORITED, ITEM_UNFAVORITED } from "../constants/actionTypes";
-import Placeholder from "../imgs/placeholder";
+import Placeholder from "../imgs/placeholder.png";
 
 const mapDispatchToProps = (dispatch) => ({
   favorite: (slug) =>
