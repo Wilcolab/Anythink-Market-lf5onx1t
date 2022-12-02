4b5c72d25a15a9a2bdf1cc2d8008d1e7d81c075a
sebastian <70783227+tamerlantian@users.noreply.github.com>
2022-12-02 14:34:04 +0000

    auto generate item images

--- backend/routes/api/items.js
+++ backend/routes/api/items.js
@@ -5,7 +5,7 @@ var Comment = mongoose.model("Comment");
 var User = mongoose.model("User");
 var auth = require("../auth");
 const { sendEvent } = require("../../lib/event");
-const { Configuration, OpenAIApi } = require("openai")
+const { Configuration, OpenAIApi } = require("openai");
 
 // Preload item objects on routes with ':item'
 router.param("item", function(req, res, next, slug) {
