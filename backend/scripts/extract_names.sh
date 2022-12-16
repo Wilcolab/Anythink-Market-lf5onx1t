c4081650b4f475613493104f6dc30fb70520474e
sebastian <70783227+tamerlantian@users.noreply.github.com>
2022-12-16 18:13:27 +0000

    adding output file

--- backend/scripts/extract_names.sh
+++ backend/scripts/extract_names.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
+#!/bin/sh
 
-cat $1 | grep -i @amazon.com | cut -d "," -f 2-3 | sed 's/,/ /g' | awk '{print $2,$1}'
+cat $1 | grep -i @amazon.com | cut -d "," -f 2-3 | sed 's/,/ /g' | awk '{print $2,$1}' > $2
