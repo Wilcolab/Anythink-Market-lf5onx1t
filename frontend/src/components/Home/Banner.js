20072b1011f2da4949275fffe3da93f390be0454
sebastian <70783227+tamerlantian@users.noreply.github.com>
2023-01-18 16:01:28 +0000

    refactoring banner background with styled-components

--- frontend/src/components/Home/Banner.js
+++ frontend/src/components/Home/Banner.js
@@ -1,18 +1,25 @@
 import React from "react";
 import logo from "../../imgs/logo.png";
+import  styled  from "styled-components";
 
 const Banner = () => {
+  const Div = styled.div`
+    background-color: ${props => props.theme.dark}
+  `;
+
   return (
-    <div className="banner text-white">
-      <div className="container p-4 text-center">
-        <img src={logo} alt="banner" />
-        <div>
-          <span>A place to </span>
-          <span id="get-part">get</span>
-          <span> the cool stuff.</span>
+    <Div>
+      <div className="banner text-white">
+        <div className="container p-4 text-center">
+          <img src={logo} alt="banner" />
+          <div>
+            <span>A place to </span>
+            <span id="get-part">get</span>
+            <span> the cool stuff.</span>
+          </div>
         </div>
       </div>
-    </div>
+    </Div>
   );
 };
 
