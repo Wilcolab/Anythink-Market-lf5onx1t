8cbc0d9b3e108062ad8e4dd2444df3c7ac453767
sebastian hoyos <sebastian.h.piedrahita@gmail.com>
2023-02-20 20:41:12 -0500

    replacing async/await for promise.all

--- frontend/src/components/Item/utils/ItemFetcher.js
+++ frontend/src/components/Item/utils/ItemFetcher.js
@@ -1,8 +1,8 @@
 import agent from "../../../agent";
 
 export async function getItemAndComments(id) {
-  const item = await agent.Items.get(id);
-  const comments = await agent.Comments.forItem(id);
+  const item =  agent.Items.get(id);
+  const comments =  agent.Comments.forItem(id);
 
-  return [item, comments];
+  return Promise.all([item, comments]).then(res => res)
 }
